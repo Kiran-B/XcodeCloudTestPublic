1a4171cadb18302b4499142de3c5f171dd76aceb
Kiran <12889736+Kiran-B@users.noreply.github.com>
2021-08-12 16:23:18 +0530

    Initial commit

---
+++ XcodeCloudTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XcodeCloudTest
+//
+//  Created by Kiran B on 8/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
